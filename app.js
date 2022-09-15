67654133f60428b6989375bb1e5988b3c41a24da
Pedro Díaz <pgdiaz.bautista@gmail.com>
2022-09-14 19:14:21 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const argv = require('./config/yargs');
+const { crearArchivo } = require('./helpers/multiplicar');
+
+require('colors');
+
+console.clear()
+
+const base = 3;
+crearArchivo(argv.b,argv.l, argv.h)
+    .then(nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
+    .catch(err => console.log(err));
+
+
